ef7a7da4f8729823b5610c5772083b5860049f38
Ky42 <ky42@protonmail.com>
2025-11-26 13:11:09 -0330

    Designed product page

--- lib/screens/product_page.dart
+++ lib/screens/product_page.dart
@@ -7,11 +7,8 @@ class ProductPage extends ConsumerWidget {
   final String id;
   final String itemType;
 
-  const ProductPage({
-    Key? key,
-    required this.id,
-    required this.itemType,
-  }) : super(key: key);
+  const ProductPage({Key? key, required this.id, required this.itemType})
+    : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -20,11 +17,134 @@ class ProductPage extends ConsumerWidget {
     return Scaffold(
       appBar: AppBar(title: Text('Product Page')),
       body: itemAsync.when(
-        data: (item) => Center(child: Text('Product Title: ${item?.title}')),
+        data: (item) {
+          if (item == null) {
+            return const Center(child: Text('Error: Item not found.'));
+          }
+          return Column(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: 250,
+                child: Image.network(item.primaryImageUrl, fit: BoxFit.cover),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 36),
+                       Text(
+                        item.title,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 20,
+                        ),
+                      ),
+                    const SizedBox(height: 8),
+                     Text(
+                        'Condition: ${item.condition.displayName}',
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey[700],
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    const SizedBox(height: 36),
+                   Text(
+                        // TODO: Change to user display name when available and add profile picture
+                        item.userId,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.w900,
+                          fontSize: 16,
+                        ),
+                      ),
+                    const SizedBox(height: 36),
+                    Text(
+                        'Description',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(8),
+                        child: Container(
+                          width: 400,
+                          color: Colors.grey[200],
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 12,
+                            horizontal: 16,
+                          ),
+                          child: Text(
+                            item.description,
+                            style: const TextStyle(fontSize: 15),
+                          ),
+                        ),
+                      ),
+                    const SizedBox(height: 36),
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // TODO: Add logic
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Icon(itemType == 'free' ? Icons.redeem : itemType == 'trade' ? Icons.swap_horiz : Icons.attach_money),
+                            SizedBox(width: 12),
+                            Text(itemType == 'free' ? 'Claim': itemType == 'trade' ? 'Trade' : 'Buy'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 12),
+                    SizedBox(
+                      width: 200,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // TODO: Add logic
+                        },
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(vertical: 14),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(8),
+                          ),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: const [
+                            Icon(Icons.message),
+                            SizedBox(width: 12),
+                            Text('Chat'),
+                          ],
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 36),
+                  ],
+                ),
+              ),
+            ],
+          );
+        },
         loading: () => const Center(child: CircularProgressIndicator()),
         error: (err, stack) => Center(child: Text('Error: $err')),
       ),
-      bottomNavigationBar: BottomNavBar(currentIndex: itemType == 'free' ? 0 : itemType == 'trade' ? 1 : 2),
+      bottomNavigationBar: BottomNavBar(
+        currentIndex: itemType == 'free'
+            ? 0
+            : itemType == 'trade'
+            ? 1
+            : 2,
+      ),
     );
   }
 }
